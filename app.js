e6253268456bcd3967f8549ac481ff89393da213
syed fahad ali <155745867+syedfahadali399@users.noreply.github.com>
2024-01-21 19:17:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var userNamed = prompt('What is Your Name')
+
+console.log(userNamed)
+
+alert('Hello' + userNamed + 'Welcome') 
+
+
+var Length = 6
+var width = 3
+
+var area = Length*width;
+
+console.log('The Area Of Triangle Is' + area)
